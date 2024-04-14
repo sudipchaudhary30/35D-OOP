a024e30cceebb61d6720e36ebcf414b68f0dcdb2
sudip <chsudip30@gmail.com>
2024-04-14 12:17:29 +0545

    Week 1 comment

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -2,6 +2,7 @@ package Week1;
 
 public class Hello{
     public static void main(String[] args) {
+         /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello World");
     }
 }
