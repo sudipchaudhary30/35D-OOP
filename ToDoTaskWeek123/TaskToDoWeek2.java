08848bb4b707a8a7e3bf2b3f00c972d0b9138466
sudip <chsudip30@gmail.com>
2024-04-30 13:52:48 +0545

    Task uncommented

--- ToDoTaskWeek123/TaskToDoWeek2.java
+++ ToDoTaskWeek123/TaskToDoWeek2.java
@@ -59,23 +59,21 @@ public class TaskToDoWeek2 {
          * and product of them.
          */
 
-        /*
-         * Scanner scanner = new Scanner(System.in);
-         * 
-         * System.out.println("Enter the first integer: ");
-         * int firstInt = scanner.nextInt();
-         * 
-         * System.out.println("Enter the second integer: ");
-         * int secondInt = scanner.nextInt();
-         * 
-         * int sum = firstInt + secondInt;
-         * System.out.println("Sum of the two integers: " + sum);
-         * 
-         * int product = firstInt * secondInt;
-         * System.out.println("Product of the two integers: " + product);
-         * 
-         * scanner.close();
-         */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first integer: ");
+        int firstInt = scanner.nextInt();
+
+        System.out.println("Enter the second integer: ");
+        int secondInt = scanner.nextInt();
+
+        int sum = firstInt + secondInt;
+        System.out.println("Sum of the two integers: " + sum);
+
+        int product = firstInt * secondInt;
+        System.out.println("Product of the two integers: " + product);
+
+        scanner.close();
 
         /*
          * 6. Take two integer inputs from the user. First, calculate the sum of two,
@@ -83,46 +81,43 @@ public class TaskToDoWeek2 {
          * sum and product and print the result.
          */
 
-        /*
-         * Scanner scanner2 = new Scanner(System.in);
-         * System.out.println("Enter the first integer: ");
-         * int firstInt = scanner2.nextInt();
-         * 
-         * System.out.println("Enter the second integer: ");
-         * int secondInt = scanner2.nextInt();
-         * 
-         * int sum = firstInt + secondInt;
-         * 
-         * int product = firstInt * secondInt;
-         * 
-         * if (product != 0) {
-         * double divisionResult = (double) sum / product;
-         * System.out.println("Division of the sum and product: " + divisionResult);
-         * } else {
-         * System.out.println("Cannot divide by zero. Product is zero.");
-         * }
-         * scanner2.close();
-         * 
-         * /*
+        
+         Scanner scanner2 = new Scanner(System.in);
+          System.out.println("Enter the first integer: ");
+          int firstInt1 = scanner2.nextInt();
+         
+         System.out.println("Enter the second integer: ");
+         int secondInt1 = scanner2.nextInt();
+          
+         int sum1 = firstInt + secondInt;
+         
+         int product1 = firstInt * secondInt;
+         
+          if (product != 0) {
+          double divisionResult = (double) sum / product;
+          System.out.println("Division of the sum and product: " + divisionResult);
+          } else {
+          System.out.println("Cannot divide by zero. Product is zero.");
+          }
+          scanner2.close();
+          
+          /*
          * 7. Take the name, roll number, and field of interest from the user and print
          * in the format below: Hey, my name is XYZ and my roll number is XYZ. My field
          * of interest are xyz.
          */
 
-        /*
-         * Scanner scan = new Scanner(System.in);
-         * System.out.println("Enter your name: ");
-         * String name = scan.nextLine();
-         * System.out.println("Enter your roll number: ");
-         * String roll = scan.nextLine();
-         * System.out.println("Enter your field of intrest: ");
-         * String intrest = scan.nextLine();
-         * 
-         * System.out.println("Hey, my name is " + name + " and my roll number is " +
-         * roll + ". My field of intrest are " + intrest +".");
-         * 
-         * scan.close();
-         */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Enter your roll number: ");
+        String roll = scan.nextLine();
+        System.out.println("Enter your field of intrest: ");
+        String intrest = scan.nextLine();
+        System.out.println("Hey, my name is " + name + " and my roll number is " +
+                roll + ". My field of intrest are " + intrest + ".");
+
+        scan.close();
 
         /*
          * 8. Take side of a square from user and print area and perimeter of it. Also,
@@ -131,29 +126,31 @@ public class TaskToDoWeek2 {
          *
          */
 
-         Scanner scan = new Scanner(System.in);
-         System.out.println("Enter the length of square: ");
-         int Squarelength = scan.nextInt();
-         int areaOfSquare = Squarelength*Squarelength;
-         int perimeterOfSquare = 4*Squarelength;
-         System.out.println("The area of square is " + areaOfSquare + " Unit^2 " + "and perimeter is " + perimeterOfSquare + " unit.");
+        System.out.println("Enter the length of square: ");
+        int Squarelength = scan.nextInt();
+        int areaOfSquare = Squarelength * Squarelength;
+        int perimeterOfSquare = 4 * Squarelength;
+        System.out.println("The area of square is " + areaOfSquare + " Unit^2 " + "and perimeter is "
+                + perimeterOfSquare + " unit.");
 
-         // 9.  Ask user to give two double input for length and breadth of a rectangle and print area type casted to int.
+        // 9. Ask user to give two double input for length and breadth of a rectangle
+        // and print area type casted to int.
         System.out.println("Enter the length of rectangle:");
         double l = scan.nextDouble();
         System.out.println("Enter the breadth of rectangle");
         double b = scan.nextDouble();
-        double  Area = l * b;
+        double Area = l * b;
         int intarea = (int) Area;
         System.out.println("The area is " + intarea);
 
-
-        // 10. Write a program to calculate the total marks of four subjects of a student and
-        // the total percentage secured. And use the following conditions to generate the final result;
+        // 10. Write a program to calculate the total marks of four subjects of a
+        // student and
+        // the total percentage secured. And use the following conditions to generate
+        // the final result;
         // a. If equal to or more than 70 -> First Class
         // b. If more than 59 -> Upper Second Class
         // c. If more than 49 -> Second class
-        // d. If more than 39 -> Third class and if below than 40 the result is fail. 
+        // d. If more than 39 -> Third class and if below than 40 the result is fail.
 
         System.out.println("enter the marks of First subject");
         int first = scan.nextInt();
@@ -163,17 +160,14 @@ public class TaskToDoWeek2 {
         int third = scan.nextInt();
         System.out.println("Enter the marks of fourth subject");
         int fourth = scan.nextInt();
-        int Total = first+second+third+fourth;
-        double Per = (Total/4.0);
+        int Total = first + second + third + fourth;
+        double Per = (Total / 4.0);
         System.out.println("Total marks " + Total);
         System.out.println("Percentage is " + Per);
-        String result = (Per>= 70) ? "First Class" :
-                        (Per> 59) ? "Upper Second Class" :
-                        (Per> 49) ? "Second Class" :
-                        (Per> 39) ? "Third Class" : "Fail";
+        String result = (Per >= 70) ? "First Class"
+                : (Per > 59) ? "Upper Second Class" : (Per > 49) ? "Second Class" : (Per > 39) ? "Third Class" : "Fail";
         System.out.println(result);
         scan.close();
-
 
     }
 
